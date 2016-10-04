38818dae12a63d89fb447a3e4d45804306321084
adongre <avinash@ampool.io>
2016-10-04 13:55:14 +0530

    Adding option for blocking and non-blocking clients.

--- bench/src/main/java/io/adongre/grpc/formatted/ScanFormattedClient.java
+++ bench/src/main/java/io/adongre/grpc/formatted/ScanFormattedClient.java
@@ -1,13 +1,12 @@
 package io.adongre.grpc.formatted;
 
-import io.adongre.grpc.generated.ColumnValues;
-import io.adongre.grpc.generated.ScanFormattedResponse;
-import io.adongre.grpc.generated.ScanFormattedRow;
-import io.adongre.grpc.generated.ScanRequest;
+import io.adongre.grpc.generated.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,54 +14,118 @@ import java.util.concurrent.TimeUnit;
  */
 public class ScanFormattedClient {
   private void scanExecute() throws InterruptedException {
-    final int NUMBER_OF_COLUMNS = 1024;
+    final int NUMBER_OF_COLUMNS = 6;
     final long NUM_OF_ROWS = 2_000_000L;
     final int SIZE_OF_EACH_COLUMN = 32;
     ManagedChannel channel = null;
+
+    final int CLIENT_BLOCKING = 1;
+    final int CLIENT_NON_BLOCKING = 2;
+
+    int clientMode = Integer.getInteger("client.blocking", CLIENT_BLOCKING);
+    int numOfIteration = Integer.getInteger("num.iteration", 5);
+
+
+    final long bytesReceived[] = {0L};
+    final long rowsReceivedNum[] = {0L};
+    final long startTime[] = {0L};
 
     try {
       channel = ManagedChannelBuilder.forAddress("localhost",
           50051)
           .usePlaintext(true)
           .executor(com.google.common.util.concurrent.MoreExecutors.directExecutor())
+          .directExecutor()
           .build();
-      io.adongre.grpc.generated.ScanServiceGrpc.ScanServiceBlockingStub blockingStub =
-          io.adongre.grpc.generated.ScanServiceGrpc.newBlockingStub(channel);
 
-      long startTime = System.nanoTime();
-      long bytesReceived = 0L;
-      long rowsReceivedNum = 0L;
-      Iterator<ScanFormattedResponse> scanResultIterator =
-          blockingStub.formatedScan(ScanRequest.newBuilder()
+      if (clientMode == CLIENT_NON_BLOCKING) {
+        System.out.println(" Using  CLIENT_NON_BLOCKING ... ");
+        final ScanServiceGrpc.ScanServiceStub asyncStub = ScanServiceGrpc.newStub(channel);
+        for ( int i = 0; i < numOfIteration; i++) {
+          startTime[0] = System.nanoTime();
+          final CountDownLatch finishLatch = new CountDownLatch(1);
+          asyncStub.formatedScan(ScanRequest.newBuilder()
               .setNumOfColumns(NUMBER_OF_COLUMNS)
               .setNumOfRows(NUM_OF_ROWS)
               .setSizeOfEachColumn(SIZE_OF_EACH_COLUMN)
-              .build());
+              .build(), new StreamObserver<ScanFormattedResponse>() {
+            @Override
+            public void onNext(ScanFormattedResponse scanFormattedResponse) {
+              bytesReceived[0] += 8; // timestamp
+              bytesReceived[0] += 8; // row id
+              Iterator<ScanFormattedRow> scanFormattedRowIterator = scanFormattedResponse.getRowList().iterator();
+              while (scanFormattedRowIterator.hasNext()) {
+                ScanFormattedRow scanFormattedRow = scanFormattedRowIterator.next();
+                Iterator<ColumnValues> columnValuesIterator = scanFormattedRow.getColumnValueList().iterator();
+                while (columnValuesIterator.hasNext()) {
+                  ColumnValues columnValues = columnValuesIterator.next();
+                  bytesReceived[0] += columnValues.getColumnName().toByteArray().length;
+                  bytesReceived[0] += columnValues.getColumnValue().toByteArray().length;
+                }
+                rowsReceivedNum[0]++;
+              }
+            }
 
-      while (scanResultIterator.hasNext()) {
-        ScanFormattedResponse scanFormattedResponse = scanResultIterator.next();
-        Iterator<ScanFormattedRow> scanFormattedRowIterator = scanFormattedResponse.getRowList().iterator();
-        while (scanFormattedRowIterator.hasNext()) {
-          ScanFormattedRow scanFormattedRow = scanFormattedRowIterator.next();
-          bytesReceived += 8; // timestamp
-          bytesReceived += 8; // row id
-          Iterator<ColumnValues> columnValuesIterator = scanFormattedRow.getColumnValueList().iterator();
-          while (columnValuesIterator.hasNext()) {
-            ColumnValues columnValues = columnValuesIterator.next();
-            bytesReceived += columnValues.getColumnName().toByteArray().length;
-            bytesReceived += columnValues.getColumnValue().toByteArray().length;
+            @Override
+            public void onError(Throwable throwable) {
+              finishLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+              double seconds = (System.nanoTime() - startTime[0]) / 1000000000.0;
+              System.out.println();
+              System.out.println("Num Of Rows     -> " + rowsReceivedNum[0]);
+              System.out.println("Time            -> " + seconds + " Seconds");
+              System.out.println("Total Data Size -> " + bytesReceived[0]);
+              System.out.println("Data Rate       -> " + ((bytesReceived[0] / 1024L / 1024L) / seconds) + " MBps");
+              System.out.println();
+              bytesReceived[0] = 0L;
+              rowsReceivedNum[0] = 0L;
+               finishLatch.countDown();
+            }
+          });
+          finishLatch.await(30, TimeUnit.MINUTES);
+        }
+      } else {
+        System.out.println(" Using  CLIENT_BLOCKING ... ");
+        io.adongre.grpc.generated.ScanServiceGrpc.ScanServiceBlockingStub blockingStub =
+            io.adongre.grpc.generated.ScanServiceGrpc.newBlockingStub(channel);
+
+        for (int i = 0; i < numOfIteration; i++) {
+          startTime[0] = System.nanoTime();
+          Iterator<ScanFormattedResponse> scanResultIterator =
+              blockingStub.formatedScan(ScanRequest.newBuilder()
+                  .setNumOfColumns(NUMBER_OF_COLUMNS)
+                  .setNumOfRows(NUM_OF_ROWS)
+                  .setSizeOfEachColumn(SIZE_OF_EACH_COLUMN)
+                  .build());
+
+          while (scanResultIterator.hasNext()) {
+            ScanFormattedResponse scanFormattedResponse = scanResultIterator.next();
+            Iterator<ScanFormattedRow> scanFormattedRowIterator = scanFormattedResponse.getRowList().iterator();
+            while (scanFormattedRowIterator.hasNext()) {
+              ScanFormattedRow scanFormattedRow = scanFormattedRowIterator.next();
+              bytesReceived[0] += 8; // timestamp
+              bytesReceived[0] += 8; // row id
+              Iterator<ColumnValues> columnValuesIterator = scanFormattedRow.getColumnValueList().iterator();
+              while (columnValuesIterator.hasNext()) {
+                ColumnValues columnValues = columnValuesIterator.next();
+                bytesReceived[0] += columnValues.getColumnName().toByteArray().length;
+                bytesReceived[0] += columnValues.getColumnValue().toByteArray().length;
+              }
+              rowsReceivedNum[0]++;
+            }
           }
-          rowsReceivedNum++;
+          double seconds = (System.nanoTime() - startTime[0]) / 1000000000.0;
+          System.out.println();
+          System.out.println("Num Of Rows     -> " + rowsReceivedNum[0]);
+          System.out.println("Time            -> " + seconds + " Seconds");
+          System.out.println("Total Data Size -> " + bytesReceived[0]);
+          System.out.println("Data Rate       -> " + ((bytesReceived[0] / 1024L / 1024L) / seconds) + " MBps");
+          System.out.println();
         }
       }
-
-      double seconds = (System.nanoTime() - startTime) / 1000000000.0;
-      System.out.println();
-      System.out.println("Num Of Rows     -> " + rowsReceivedNum);
-      System.out.println("Time            -> " + seconds + " Seconds");
-      System.out.println("Total Data Size -> " + bytesReceived);
-      System.out.println("Data Rate       -> " + ((bytesReceived / 1024L / 1024L) / seconds) + " MBps");
-      System.out.println();
     } finally {
       if (channel != null) {
         try {
